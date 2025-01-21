ad7710c748e1484db05e57d0301c8f0c9751fdd4
nohwolde <58862403+nohwolde@users.noreply.github.com>
2025-01-21 00:10:17 -0500

    Adding Umami analytics to prepare for public launch

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -16,6 +16,7 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
+      <script defer src="https://cloud.umami.is/script.js" data-website-id="e2e5fb42-7329-49ae-a205-f217743f69c3"></script>
       <body className={inter.className}>{children}</body>
     </html>
   )
